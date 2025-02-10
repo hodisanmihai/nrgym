36c955b1c35a653db1c7998b529ad7a87881f4d5
hodisanmihai <mihaihodisn@gmail.com>
2025-02-10 17:35:36 +0200

    save

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import logo from "./assets/photos/logo_nrgym_header.svg"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -13,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "NRGYM",
+  title: "NRGym - Sala de Fitness din Oradea",
   description: "NRGym este sala de fitness din Oradea care oferă antrenamente personalizate, echipamente moderne și un mediu motivant pentru a-ți atinge obiectivele de sănătate și fitness. Vino să experimentezi sesiuni de antrenament eficiente, ghidate de instructori calificați, într-un spațiu prietenos și energic. Fie că vrei să slăbești, să îți îmbunătățești condiția fizică sau să construiești masă musculară, NRGym este locul perfect pentru tine. Începe azi călătoria ta către un stil de viață mai sănătos!",
 };
 
@@ -29,6 +30,16 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <head>
+  <link rel="icon" href={logo ?? '/default-logo.svg'} type="image/svg+xml" /> {/* Asigură-te că folosești un fallback */}
+  
+  <meta name="description" content={metadata.description || ''} /> {/* Folosește o valoare implicită dacă description este null sau undefined */}
+  
+  {/* Alte meta taguri */}
+  <meta name="robots" content="index, follow" />
+  <meta property="og:title" content="NRGym - Sala de Fitness din Oradea" />
+  <meta property="og:description" content={metadata.description || ''} />
+</head>
       </body>
     </html>
   );
